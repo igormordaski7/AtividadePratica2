c96bed9a19743ea48f9379a357693e7a8e63310e
igormordaski7 <144966766+igormordaski7@users.noreply.github.com>
2024-04-17 19:10:35 -0300

    Create Moto.java
    Adicionando a subclasse Moto.java

---
+++ Moto.java
@@ -0,0 +1,26 @@
+public class Moto extends Veiculo {
+
+    private int cilindrada;
+
+    public Moto(String marca, String modelo, int anoFabricacao, int cilindrada) {
+        super(marca, modelo, anoFabricacao);
+        this.cilindrada = cilindrada;
+    }
+
+    public int getCilindrada() {
+        return cilindrada;
+    }
+
+    public void setCilindrada(int cilindrada) {
+        this.cilindrada = cilindrada;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + 
+        "Cilindrada: " + cilindrada;
+    }
+
+    
+    
+}
